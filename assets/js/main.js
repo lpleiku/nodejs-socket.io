eb36bd6b7b519e8c04a4d2a39913fe32a3b3943e
lpleiku <vuducnam2711@gmail.com>
2018-05-23 22:03:33 +0700

    create + join room

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,21 @@
-var socket = io('https://chatpk.herokuapp.com/');
+// var socket = io('https://chatpk.herokuapp.com/');
+var socket = io('http://localhost:3000/');
 
 $(document).ready(function(){
     var alert_name = $('.formName .alert-danger'),
         input_name = $('.formName #inputName'),
         list_member = $('.listMember'),
         input_message = $('#inputMessage'),
-        list_message = $('.message')
+        list_message = $('.message'),
+        input_room = $('#input-room'),
+        btn_room = $('.create-room'),
+        alert_modal = $('.modal-body .alert-danger'),
+        close_modal = $('.modal-footer .btn-close'),
+        list_room = $('.listRoom'),
+        room = $('.listRoom .room')
     ;
 
+    //create name
     input_name.keypress(function (e) {
         if(e.which === 13){
             var name_val = $(this).val();
@@ -24,6 +32,7 @@ $(document).ready(function(){
         }
     });
 
+    //send message
     input_message.keypress(function (e) {
         if(e.which === 13){
             var message_val = $(this).val();
@@ -31,6 +40,21 @@ $(document).ready(function(){
                 socket.emit('message', message_val);
                 input_message.val('');
             }
+        }
+    });
+
+    // create room
+    btn_room.on('click', function () {
+        var room_val = input_room.val();
+        if(room_val === ''){
+            alert_modal.removeClass('d-none');
+            setTimeout(function(){
+                alert_name.addClass("d-none");
+            }, 2000);
+            alert_modal.html('Room is required!');
+        }
+        else {
+            socket.emit('create_room', room_val);
         }
     });
 
@@ -42,9 +66,10 @@ $(document).ready(function(){
         alert_name.html('This name already has the user!');
     });
 
-    socket.on('username_success', function () {
+    socket.on('username_success', function (username) {
         $('.formName').hide();
         $('.homePage').show();
+        $('.info span').html(username);
     });
 
     socket.on('listUser', function (listUser) {
@@ -59,4 +84,28 @@ $(document).ready(function(){
         list_message.scrollTop(list_message[0].scrollHeight);
 
     });
+
+    socket.on('create_room_error', function () {
+        alert_modal.removeClass('d-none');
+        setTimeout(function(){
+            alert_modal.addClass("d-none");
+        }, 2000);
+        alert_modal.html('This room is exists!');
+    });
+
+    socket.on('create_room_success', function (room) {
+        console.log('success: ' + room);
+        close_modal.click();
+    });
+
+    socket.on('listRoom', function (listRoom) {
+        list_room.children().remove();
+        listRoom.forEach(function(item) {
+            list_room.append('<div class="room" onclick="join_room(\'' + item.name + '\')">' + item.name + '(' + item.number + ')</div>')
+        });
+    });
 });
+
+function join_room(room) {
+    socket.emit('join_room', room);
+}
--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 var express = require('express');
-
 var app = express();
+
 app.use(express.static('assets'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
+
 server.listen(port, function () {
     console.log('App listening on port 3000')
 });
 
 var listUser = [];
+var listRoom = [];
 
 io.on('connection', function (socket) {
 
     socket.on('disconnect', function () {
-        listUser.splice(listUser.indexOf(socket.username), 1);
+        if(listUser.indexOf(socket.username) >= 0){
+            listUser.splice(listUser.indexOf(socket.username), 1);
+        }
         io.sockets.emit('listUser', listUser);
+
+        if(!socket.adapter.rooms[socket.room]){
+            if(listRoom.findIndex(function(r){return r.name === socket.room}) >= 0){
+                listRoom.splice(listRoom.findIndex(function(r){return r.name === socket.room}), 1);
+            }
+        }
+        else {
+            listRoom[listRoom.findIndex(function(r){return r.name === socket.room})].number = socket.adapter.rooms[socket.room].length;
+        }
+        io.sockets.emit('listRoom', listRoom);
     });
 
     socket.on('username', function (username) {
@@ -27,13 +42,34 @@ io.on('connection', function (socket) {
         else {
             listUser.push(username);
             socket.username = username;
-            socket.emit('username_success');
+            socket.emit('username_success', username);
             io.sockets.emit('listUser', listUser);
+            io.sockets.emit('listRoom', listRoom);
         }
     });
 
     socket.on('message', function (message) {
         io.sockets.emit('resMessage', {user: socket.username, mess: message});
+    });
+
+    socket.on('create_room', function (room) {
+        if(listRoom.indexOf(room) >= 0){
+            socket.emit('create_room_error');
+        }
+        else {
+            socket.join(room);
+            socket.room = room;
+            listRoom.push({name: room, number: socket.adapter.rooms[room].length});
+            socket.emit('create_room_success', room);
+            io.sockets.emit('listRoom', listRoom);
+        }
+    });
+
+    socket.on('join_room', function (room) {
+        socket.join(room);
+        socket.room = room;
+        listRoom[listRoom.findIndex(function(r){return r.name === room})].number = socket.adapter.rooms[room].length;
+        io.sockets.emit('listRoom', listRoom);
     });
 });
 
